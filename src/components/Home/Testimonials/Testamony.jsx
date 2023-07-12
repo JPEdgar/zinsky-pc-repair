4c2b2493584e06a0bb48ed83691074007ef05f43
JPEdgar <Jon.P.Edgar@gmail.com>
2023-07-12 08:10:54 -0700

    Minor formatting on testamonies

--- src/components/Home/Testimonials/Testamony.jsx
+++ src/components/Home/Testimonials/Testamony.jsx
@@ -5,12 +5,14 @@ import { Col } from "react-bootstrap";
 const Testamony = ({ data }) => {
   return (
     <Col className="mt-1" xs={12} md={6}>
-      <div>From: {data.name}</div>
-      {data.data.map((testamony, index) => (
-        <div key={`testamony-data-${index}`} className="mb-2">
-          {testamony}
-        </div>
-      ))}
+      <div className="fs-4">From: {data.name}</div>
+      <i>
+        {data.data.map((testamony, index) => (
+          <div key={`testamony-data-${index}`} className="mb-2">
+            {testamony}
+          </div>
+        ))}
+      </i>
     </Col>
   );
 };
