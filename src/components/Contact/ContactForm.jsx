dfc47cd643a00ffc02eb52b92d73c577f6622bc9
JPEdgar <Jon.P.Edgar@gmail.com>
2023-12-07 20:27:18 -0800

    Added recaptcha

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 import { Form, Button, Row, Col } from "react-bootstrap";
+import ReCAPTCHA from "react-google-recaptcha";
 
 import { useFormContext } from "../../context/FormContext";
 
 const ContactForm = () => {
+  const key = "6LerTyopAAAAAC_b7F0PwU6i6e-R9rgneKWBqEI4";
   const { formInfo, setFormInfo, clearForm } = useFormContext();
+  const [recap, setRecap] = useState(null);
+
   const handleChange = (e) => {
     setFormInfo((curr) => ({ ...curr, [e.target.name]: e.target.value }));
   };
@@ -15,9 +19,18 @@ const ContactForm = () => {
     setFormInfo((curr) => ({ ...curr, [e.target.name]: value }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("here")
+  }
+
+  const handleRecaptcha = (capData) => {
+    setRecap(capData)
+  }
+
   return (
     <>
-      <Form style={{ minHeight: "100vh" }}>
+      <Form style={{ minHeight: "100vh" }} onSubmit={handleSubmit}>
         <Form.Group as={Row} className="my-2">
           <Col xs="12" md="6">
             <Form.Label className="mb-0">First Name</Form.Label>
@@ -111,11 +124,14 @@ const ContactForm = () => {
           </Form.Group>
         </Form.Group>
 
+        {!recap && <ReCAPTCHA sitekey={key} onChange={(capData) => handleRecaptcha(capData)} />}
+
         <div className="mb-2">
           <Button
             variant="primary"
-            // type="submit" // production
-            type="button" // dev
+            type="submit" // production
+            
+            disabled={!recap}
             className="me-2"
             style={{ minWidth: "100px" }}
           >
